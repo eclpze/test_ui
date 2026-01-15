bf1ab05baaa057aa187620bb17423746b3bd22ef
Камилла Губайдуллина <gubai.k60@gmail.com>
2026-01-15 09:50:11 +0500

    Проверка библиотеки

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,38 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:new_ui/new_ui.dart';
 import 'package:test_ui/presentation/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('TextField Error', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // При нажатии на кнопку проверяем валидацию данных
+    await tester.tap(find.text('Отправить'));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Появляется ошибка, т.к. текстовое поле пустое
+    expect(find.text('Введите имя'), findsOneWidget);
+
+
+    // Вводим нужные данные, нажимаем кнопку для проверки
+    final nameTextField = find.widgetWithText(CustomTextField, 'Имя');
+    await tester.enterText(nameTextField, 'Камилла');
+    await tester.tap(find.text('Отправить'));
+    await tester.pump();
+
+    // Данные в поле верны, ошибка исчезает
+    expect(find.text('Введите имя'), findsNothing);
   });
+
+  // testWidgets('', (WidgetTester tester) async {
+  //   await tester.pumpWidget(const MyApp());
+  //
+  //   expect(find.text('0'), findsOneWidget);
+  //
+  //   await tester.tap(find.byIcon(Icons.add));
+  //   await tester.pump();
+  //
+  //   expect(find.text('1'), findsOneWidget);
+  // });
 }
